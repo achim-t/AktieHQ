51e2510f670f4378afe190fbf5ecb29d6da968cf
Achim Taeschner <ataeschner@LeBit.com>
2016-04-27 15:48:19 +0200

    init

---
+++ app/src/main/java/de/programmierenlernenhq/aktiehq/AktienlisteFragment.java
@@ -0,0 +1,95 @@
+package de.programmierenlernenhq.aktiehq;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AktienlisteFragment extends Fragment{
+
+    public AktienlisteFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        String LOG_TAG = AktienlisteFragment.class.getSimpleName();
+
+        Log.v(LOG_TAG, "verbose     - Meldung");
+        Log.d(LOG_TAG, "debug       - Meldung");
+        Log.i(LOG_TAG, "information - Meldung");
+        Log.w(LOG_TAG, "warning     - Meldung");
+        Log.e(LOG_TAG, "error       - Meldung");
+
+        String [] aktienlisteArray = {
+                "Adidas - Kurs: 73,45 €",
+                "Allianz - Kurs: 145,12 €",
+                "BASF - Kurs: 84,27 €",
+                "Bayer - Kurs: 128,60 €",
+                "Beiersdorf - Kurs: 80,55 €",
+                "BMW St. - Kurs: 104,11 €",
+                "Commerzbank - Kurs: 12,47 €",
+                "Continental - Kurs: 209,94 €",
+                "Adidas - Kurs: 73,45 €",
+                "Allianz - Kurs: 145,12 €",
+                "BASF - Kurs: 84,27 €",
+                "Bayer - Kurs: 128,60 €",
+                "Beiersdorf - Kurs: 80,55 €",
+                "BMW St. - Kurs: 104,11 €",
+                "Commerzbank - Kurs: 12,47 €",
+                "Continental - Kurs: 209,94 €",
+                "Daimler - Kurs: 84,33 €"
+        };
+        List<String> aktienListe = new ArrayList<>(Arrays.asList(aktienlisteArray));
+
+        ArrayAdapter <String> aktienlisteAdapter =
+                new ArrayAdapter<>(
+                        getActivity(),
+                        R.layout.list_item_aktienliste,
+                        R.id.list_item_aktienliste_textview,
+                        aktienListe);
+
+        View rootView = inflater.inflate(R.layout.fragment_aktienliste, container, false);
+
+        ListView aktienlisteListView = (ListView) rootView.findViewById(R.id.listview_aktienliste);
+        aktienlisteListView.setAdapter(aktienlisteAdapter);
+
+
+        return rootView;
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_aktienlistefragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_daten_aktualisieren) {
+            Toast.makeText(getActivity(), "Aktualisieren gedrückt!", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
